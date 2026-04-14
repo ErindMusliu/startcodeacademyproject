4b6bf3016cc772db889b504c983f465d722a9521
ErindMusliu <erindmusliu.11@gmail.com>
2026-04-14 19:01:12 +0200

    Ndryshim

--- Routes/web.php
+++ Routes/web.php
@@ -3,10 +3,13 @@ use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
 use App\Controllers\HomeController;
 use App\Controllers\AdminController;
+use App\Controllers\CategoryController;
 
 return simpleDispatcher(function (RouteCollector $r) {
     $r->addRoute('GET', '/', [HomeController::class, 'index']);
     // admin
     $r->addRoute('GET', '/dashboard', [AdminController::class, 'index']);
+    // categories
+    $r->addRoute('GET', '/categories/create', [CategoryController::class, 'create']);
 
 });
