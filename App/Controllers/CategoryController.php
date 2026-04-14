16c01cd20289eaef04fc14aaf1daf70bbcaa2f3d
ErindMusliu <erindmusliu.11@gmail.com>
2026-04-14 19:23:45 +0200

    Shtova function store

--- App/Controllers/CategoryController.php
+++ App/Controllers/CategoryController.php
@@ -7,4 +7,8 @@ class CategoryController extends BaseController{
     public function create(){
         return $this->view('categories/create');
     }
+
+    public function store(){
+        var_dump($_POST['emri_kategorise'],$_POST['pershkrimi_kategorise']);
+    }
 }
