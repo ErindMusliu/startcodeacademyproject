7a9b4e56aec5db83655a3f63d24a56222c0793c8
ErindMusliu <erindmusliu.11@gmail.com>
2026-04-16 18:23:42 +0200

    Validova dhe pastrova inputet

--- App/Controllers/CategoryController.php
+++ App/Controllers/CategoryController.php
@@ -9,6 +9,29 @@ class CategoryController extends BaseController{
     }
 
     public function store(){
-        var_dump($_POST['emri_kategorise'],$_POST['pershkrimi_kategorise']);
+        $error = 0;
+        // Emri i Kategorise
+
+        $emri_i_kategorise = htmlspecialchars(strip_tags(trim($_POST['emri_kategorise'])));
+
+        if(mb_strlen($emri_i_kategorise) < 3){
+            $error_emri_kategorise = "Ju lutemi te shenoni se paku 3 karaktere";
+            ++$error;
+        }
+
+        // Pershkrimi i Kategorise
+
+        $pershkrimi_i_kategorise = htmlspecialchars(strip_tags(trim($_POST['pershkrimi_kategorise'])));
+
+        if(mb_strlen($pershkrimi_i_kategorise) < 3){
+            $error_pershkrimi_kategorise = "Ju lutemi te shenoni se paku 3 karaktere";
+            ++$error;
+        }
+
+        // Error
+
+        if($error > 0){
+            return $this->view('categories/create',['error_emri_kategorise'=>$error_emri_kategorise,'error_pershkrimi_kategorise'=>$error_pershkrimi_kategorise]);
+        }
     }
 }
