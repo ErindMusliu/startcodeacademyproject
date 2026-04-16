954a4db2e32208b66c0d96f5e88ea18e9997b732
ErindMusliu <erindmusliu.11@gmail.com>
2026-04-16 18:46:30 +0200

    E bera qe te dhenat te shkojne ne databaze

--- App/Controllers/CategoryController.php
+++ App/Controllers/CategoryController.php
@@ -33,5 +33,20 @@ class CategoryController extends BaseController{
         if($error > 0){
             return $this->view('categories/create',['error_emri_kategorise'=>$error_emri_kategorise,'error_pershkrimi_kategorise'=>$error_pershkrimi_kategorise]);
         }
+
+        else{
+            $sql = "insert into kategorite(emri_kategorise,pershkrimi_kategorise) value('$emri_i_kategorise','$pershkrimi_i_kategorise')";
+            $result = $this->conn->query($sql);
+
+            if($result){
+                $flash_success = "Te dhenat u ruajten me sukses";
+                return $this->view('categories/create',['flash_success'=>$flash_success]);
+            }
+
+            else{
+                $flash_error = "Te dhenat nuk u ruajten me sukses";
+                return $this->view('categories/create',['flash_error'=>$flash_error]);
+            }
+        }
     }
 }
