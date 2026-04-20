5f4e89502a5d63cf6d90a558e60cbef63bf8f7a7
ErindMusliu <erindmusliu.11@gmail.com>
2026-04-20 18:08:11 +0200

    Shtova function update

--- App/Controllers/CategoryController.php
+++ App/Controllers/CategoryController.php
@@ -71,4 +71,47 @@ class CategoryController extends BaseController{
             return $this->view('categories/edit',['category'=>$category]);
         }
     }
+
+    public function update($id){
+        $category_id = filter_var($id,FILTER_SANITIZE_NUMBER_INT);
+        $error = 0;
+        // Emri i Kategorise
+
+        $emri_i_kategorise = htmlspecialchars(strip_tags(trim($_POST['emri_kategorise'])));
+
+        if(mb_strlen($emri_i_kategorise) < 3){
+            $error_emri_kategorise = "Ju lutemi te shenoni se paku 3 karaktere";
+            ++$error;
+        }
+
+        // Pershkrimi i Kategorise
+
+        $pershkrimi_i_kategorise = htmlspecialchars(strip_tags(trim($_POST['pershkrimi_kategorise'])));
+
+        if(mb_strlen($pershkrimi_i_kategorise) < 3){
+            $error_pershkrimi_kategorise = "Ju lutemi te shenoni se paku 3 karaktere";
+            ++$error;
+        }
+
+        // Error
+
+        if($error > 0){
+            return $this->view('categories/edit',['error_emri_kategorise'=>$error_emri_kategorise,'error_pershkrimi_kategorise'=>$error_pershkrimi_kategorise]);
+        }
+
+        else{
+            $sql = "update kategorite set emri_kategorise='$emri_i_kategorise',pershkrimi_kategorise='$pershkrimi_i_kategorise' where id='$category_id'";
+            $result = $this->conn->query($sql);
+
+            if($result){
+                $flash_success = "Te dhenat u perditesuan me sukses";
+                return $this->view('categories/edit',['flash_success'=>$flash_success]);
+            }
+
+            else{
+                $flash_error = "Te dhenat nuk u perditesuan";
+                return $this->view('categories/edit',['flash_error'=>$flash_error]);
+            }
+        }
+    }
 }
